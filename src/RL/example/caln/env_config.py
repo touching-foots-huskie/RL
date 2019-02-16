d0e11a5fde5569724ee2b683ce858417c52bc4b4
Haonan Chang <chnme40cs@gmail.com>
2019-02-16 16:01:23 -0500

    reconfig structure

---
+++ src/RL/example/caln/env_config.py
@@ -0,0 +1,83 @@
+# Author: Harvey Chang
+# Email: chnme40cs@gmail.com
+# this is the test version of config:
+# add path:
+import sys
+sys.path.append('/mnt/storage/codes/Harvey')
+from RL.configure import configure
+
+
+class EnvConfig(configure.SubConfig):
+    def __init__(self, name):
+        configure.SubConfig.__init__(self, name)
+        # core config for mujoco
+        self.get_knowledge()
+        self.get_data()
+
+    def get_data(self):
+        self.data['environment'] = ''
+        self.data['attribute_num'] = 0
+        self.data['base'] = ''
+
+    def get_knowledge(self):
+        # knowledge
+        # the list for base task choice
+        self.knowledge['base'] = ['ball', 'arm', '3darm']
+        self.knowledge['attribute_0'] = ['safety', 'door', 'speed', 'force']
+        # dim_information
+        self.knowledge['action_dims'] = {'ball': 2, 'arm':5, '3darm':5}
+        self.knowledge['dim_lists'] = {'ball': '4,4', 'arm': '10,4', '3darm': '10,6', 'safety': ',4',
+                                       'door': ',2', 'speed': ',2', 'force': ',2'}
+
+    def refresh(self, name=None):
+        # attribute:
+        if name == 'attribute_num':
+            try:
+                self.data['attribute_num'] = int(self.data['attribute_num'])
+                for i in range(self.data['attribute_num']):
+                    self.data['attribute_{}'.format(i)] = ''
+                    # add dynamic knowledge
+                    self.knowledge['attribute_{}'.format(i)] = self.knowledge['attribute_0']
+
+                num = self.data['attribute_num']
+
+                while ('attribute_{}'.format(num) in self.data):
+                    del self.data['attribute_{}'.format(num)]
+                    num += 1
+            except:
+                print('attribute_num is not int')
+
+        elif name == 'base':
+            # when chosen base:
+            self.data['action_dim'] = self.knowledge['action_dims'][self.data['base']]
+            self.data['dim_list'] = self.knowledge['dim_lists'][self.data['base']]
+
+        elif name[:9] == 'attribute':
+            # then dim
+            i = 0
+            try:
+                self.data['dim_list'] = self.knowledge['dim_lists'][self.data['base']]
+                while('attribute_{}'.format(i) in self.data.keys()):
+                    self.data['dim_list'] += self.knowledge['dim_lists'][self.data['attribute_{}'.format(i)]]
+                    i += 1
+            except:
+                print('Choose base first')
+        else:
+            pass
+
+        name_list = self.data['base']
+        i = 0
+        while ('attribute_{}'.format(i) in self.data.keys()):
+            name_list += ',{}'.format(self.data['attribute_{}'.format(i)])
+            i += 1
+        self.data['environment'] = name_list
+
+    def push(self, updata):
+        self.data['reset_from_pool'] = False
+        # push
+        for name, value in self.data.items():
+            updata[name] = value
+
+
+if __name__ == '__main__':
+    EnvConfig('environment')
---
+++ src/RL/example/caln/main.py
@@ -0,0 +1,109 @@
+# Author: Harvey Chang
+# Email: chnme40cs@gmail.com
+import sys
+sys.path.append('/mnt/storage/codes/Harvey')
+
+import RL as rl
+import tensorflow as tf
+from RL.example.caln.policy_config import PolicyConfig
+from RL.example.caln.env_config import EnvConfig
+# environment:
+from RL.env.mujoco.env import env
+from RL.env.mujoco.protocol import Container
+# policy
+from RL.nn.ppo import PolicyRep
+from RL.train.judge import judge_wrapper
+from RL.train.update import update_wrapper
+from RL.train.evaluate import eval_wrapper
+from RL.train.simulate import *
+from RL.train.watch_dog import WatchDog
+import RL.train.optim as optim
+from collections import deque
+import numpy as np
+
+
+def update_policy(results, myconfig, mypolicy):
+    for part in myconfig['update_list']:
+        eval('optim.process_{}(results, myconfig, mypolicy)'.format(part))
+    # record in the tensorboard: results is a variant factor
+    mypolicy.log(results)
+
+
+def main():
+    # get config
+    watch_dog = WatchDog()
+    whole_config = rl.configure.configure.Config()
+    my_policy_config = PolicyConfig('policy')
+    my_env_config = EnvConfig('environment')
+    whole_config.add(my_env_config)
+    whole_config.add(my_policy_config)
+    # visualization
+    rl.configure.visual_tool.major_pane(whole_config)
+    # get env:
+    myenv = env(whole_config['environment'])
+    mycontainer = Container(whole_config['attribute_num'])
+    # get network:
+    mypolicy = PolicyRep(whole_config)
+    # init:
+    mypolicy.sess.run(tf.global_variables_initializer())
+    # flag:
+    flag = False
+    judge_func = judge_wrapper(whole_config)
+    update_func = update_wrapper(whole_config)
+    eval_func = eval_wrapper(whole_config)
+    # long_term judge
+    long_term_performance = deque([0.0]*whole_config['long_term_batch'],
+                                  maxlen=whole_config['long_term_batch'])
+
+    # restore:
+    mypolicy.restore()
+    if whole_config['mode'] != 'test':
+        # train mode
+        while not flag:
+            results = run_policy(whole_config, myenv, mycontainer, mypolicy)
+            eval_flag, average_performance = eval_func(results, whole_config, long_term_performance)
+            if eval_flag or whole_config['all_passed'] or whole_config['all_zero']:
+                update_func(whole_config)
+                long_term_performance = deque([0.0] * whole_config['long_term_batch'],
+                                              maxlen=whole_config['long_term_batch'])
+                watch_dog.refresh()
+            # update param_meter:
+            else:
+                update_policy(results, whole_config, mypolicy)
+
+            # watch dog start:
+            if not watch_dog.check(average_performance):
+                # if check false:
+                if whole_config['random_level'] == 0.1:
+                    # which means first start:
+                    # mypolicy.restart_part()
+                    mypolicy.refresh_sigma()
+                else:
+                    mypolicy.refresh_sigma()
+                # refresh all:
+                long_term_performance = deque([0.0] * whole_config['long_term_batch'],
+                                              maxlen=whole_config['long_term_batch'])
+                whole_config['reset_from_pool'] = False
+                print('watch dog start, thread restart')
+
+            whole_config['global_step'] += 1
+            flag = judge_func(whole_config)
+
+        # save:
+        mypolicy.save()
+    else:
+        results = run_policy(whole_config, myenv, mycontainer, mypolicy)
+        # make video: multi-attribute have not write.
+        if whole_config['attribute_num']:
+            add_state_list = [results['addition_{}'.format(i)] for i in range(whole_config['attribute_num'])]
+            add_state_list = np.stack(add_state_list).transpose([1, 0, 2])
+        else:
+            add_state_list = []
+        myenv.get_video('test', results['states'], results['targets'], add_state_list)
+        # print performance:
+        print('the performance is {}'.format(np.mean(results['eval_value'])))
+    print('Process end!')
+
+
+if __name__ == '__main__':
+    main()
